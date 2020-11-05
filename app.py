21d7aeb467ef9a61fe2309beff8ad5e47718b41d
danicrg <Carlander.daniel@gmail.com>
2020-11-05 12:34:09 +0100

    ♻️ Change Spanish to English

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import pandas as pd
 st.title('Should I buy a new coffee machine? :coffee:')
 st.image('./coffee.jpg', use_column_width=True)
 
-st.subheader(':page_with_curl: Datos de la cafetera actual')
+st.subheader(':page_with_curl: My current Coffee Machine')
 
-per_coffee_cost = st.number_input('Coste por café (€)', 0.0, 10.0, 0.40)
-n_coffees_per_day = st.slider('Cafés diarios', 1, 20, 7)
+per_coffee_cost = st.number_input('Coste per coffee (€)', 0.0, 10.0, 0.40)
+n_coffees_per_day = st.slider('Coffees per day', 1, 20, 7)
 
-st.subheader(':page_facing_up: Datos de la [nueva cafetera](https://amzn.to/2HbXDtn)')
+st.subheader(':page_facing_up: [New Coffee Machine](https://amzn.to/2HbXDtn)')
 
-machine_cost = st.number_input('Coste de la nueva cafetera (€)', 0.0, 2000.0, 200.0)
-coffee_bag_cost = st.number_input('Coste de una bolsa de granos de café (€)', 0.0, 30.0, 5.0)
-n_servins_per_bag = st.number_input('Número de tazas por bolsa', 0, 400, 100)
-N_YEARS_FORWARD = int(st.slider('Vida útil de la cafetera (años)', 10, 40, 20))
+machine_cost = st.number_input('New coffee machine cost (€)', 0.0, 2000.0, 200.0)
+coffee_bag_cost = st.number_input('Cost of a bag of coffee grain (€)', 0.0, 30.0, 5.0)
+n_servins_per_bag = st.number_input('Number of coffees per bag', 0, 400, 100)
+N_YEARS_FORWARD = int(st.slider('Lifespan of coffee machine (years)', 10, 40, 20))
 
 new_per_coffee_cost = coffee_bag_cost / n_servins_per_bag
 times_cheaper = -(-per_coffee_cost/new_per_coffee_cost)
@@ -23,15 +23,15 @@ times_cheaper = -(-per_coffee_cost/new_per_coffee_cost)
 amortization_time = int((machine_cost + -new_per_coffee_cost*n_coffees_per_day)//((per_coffee_cost-new_per_coffee_cost)*n_coffees_per_day))
 
 if amortization_time <= 0:
-	st.markdown('No compensa la compra.')
+	st.markdown('The buy of a new machine would mean higher costs.')
 else:
 
-	st.markdown(f'Cada taza cuesta **{round(new_per_coffee_cost, 3)} €** en la nueva máquina, **{round(times_cheaper, 1)} veces más barato** que con tu máquina actual.')
+	st.markdown(f'Each coffee costs **{round(new_per_coffee_cost, 3)} €** with the new machine, **{round(times_cheaper, 1)} times cheaper** than with your current machine.')
 
-	st.subheader(':chocolate_bar: Amortización de la compra')
-	st.markdown('- **Línea verde:** Gasto acumulado de la cafetera nueva. Comienza en el precio de la cafetera.\n' + 
-		'- **Línea roja:** Gasto acumulado de la cafetera vieja.')
-	st.markdown('En el momento en el que se cruzan las rectas es cuando la cafetera está amortizada y comienzas a ahorrar.')
+	st.subheader(':chocolate_bar: Repayment of the buy')
+	st.markdown('- **Green Line:** Cummulative cost of new machine. It starts with the machine cost.\n' + 
+		'- **Red Line:** Cummulative cost of the current machine.')
+	st.markdown('When the lines cross you start saving.')
 
 
 	N_POINTS = 365*N_YEARS_FORWARD
@@ -51,30 +51,30 @@ else:
 		"layer": [{
 			"mark": {"type": "line", "color": "#85C5A6"},
 			"encoding": {
-				"x": {"field": "days", "type": "quantitative", "title": "Días"},
-				"y": {"field": "new_cost", "type": "quantitative", "title": ""}
+				"x": {"field": "days", "type": "quantitative", "title": "Days"},
+				"y": {"field": "new_cost", "type": "quantitative", "title": "New Cummulative Cost"}
 			}
 		}, {
 	    	"mark": {"type": "line", "color": "#FF6961"},
 			"encoding": {
 				"x": {"field": "days", "type": "quantitative"},
-				"y": {"field": "actual_cost", "type": "quantitative", "title": ""}
+				"y": {"field": "actual_cost", "type": "quantitative", "title": "Current Cummulative Cost"}
 			}
 		}]
 	}, use_container_width=True)
 
 
-	st.markdown(f'En **{round(amortization_time)} días** estará amortizada la cafetera.')
+	st.markdown(f'In **{round(amortization_time)} days** you will have repayed the new machine.')
 
-	st.subheader(':euro: Ahorro por la nueva cafetera')
+	st.subheader(':euro: Savings calculations')
 
 	st.vega_lite_chart(data[:int(amortization_time*2)], {
 		"width": "container",
 	  	"height": 300,
 		"mark": "line",
 		"encoding": {
-			"x": {"field": "days", "type": "quantitative", "title": "Días"},
-			"y": {"field": "savings", "type": "quantitative", "title": "Dinero ahorrado (€)"}
+			"x": {"field": "days", "type": "quantitative", "title": "Days"},
+			"y": {"field": "savings", "type": "quantitative", "title": "Money saved (€)"}
 		}
 		
 	}, use_container_width=True)
@@ -82,9 +82,9 @@ else:
 	total_savings = data['savings'].values[-1]
 	average_savings = total_savings/N_YEARS_FORWARD
 
-	st.markdown(f'Cada año ahorrarás de media **{round(average_savings, 2)} €**')
+	st.markdown(f'Each year you will save in average **{round(average_savings, 2)} €**')
 
-	st.markdown(f'En la vida útil de la máquina ({N_YEARS_FORWARD} años) ahorrarás **{round(total_savings, 2)} €**')
+	st.markdown(f'In the lifespan of the machine ({N_YEARS_FORWARD} años) you will save **{round(total_savings, 2)} €**')
 
 	st.markdown('------')
 	st.markdown(':computer: by [Daniel Carlander](https://danielcarlander.com/?utm_source=coffee-rentability-dashboard&utm_medium=referral). Code available in [GitHub](https://github.com/danicrg/coffee-rentability)')
